dc9c0b4736b2e63b8076b55dff7afc34eea58cf2
AleksGrig <ullitap@gmail.com>
2022-11-06 20:52:06 +0300

    frames

---
+++ webelements_practice/frames.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+
+service_obj = Service("drivers/chromedriver_win32/chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(service=service_obj,options=options)
+driver.implicitly_wait(10)
+driver.maximize_window()
+
+driver.get("https://www.selenium.dev/selenium/docs/api/java/index.html?overview-summary.html")
+
+# switch_to.frame(name of the frame)
+# switch_to.frame(id of the frame)
+# switch_to.frame(webelement)
+# switch_to.frame(0) -> in case of a single frame
+
+# tag could be frame, iframe and form
+
+# switch with frame name
+driver.switch_to.frame("classFrame")
+driver.find_element(By.LINK_TEXT, "org.openqa.selenium").click()
+
+# switch back to the page
+driver.switch_to.default_content()
+
+# switch to second frame
+driver.switch_to.frame("packageFrame")
+driver.find_element(By.LINK_TEXT, "Action").click()
---
+++ webelements_practice/inner_frames.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+
+service_obj = Service("drivers/chromedriver_win32/chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(service=service_obj,options=options)
+driver.implicitly_wait(10)
+driver.maximize_window()
+
+driver.get("https://demo.automationtesting.in/Frames.html")
+
+driver.find_element(By.LINK_TEXT, "Iframe with in an Iframe").click()
+
+# switch to outer frame
+driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@src='MultipleFrames.html']"))
+
+# switch to inner frame
+driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@src='SingleFrame.html']"))
+
+driver.find_element(By.XPATH, "//input[@type='text']").send_keys("Hi there!")
+
+# switch back to parent frame
+driver.switch_to.parent_frame()
