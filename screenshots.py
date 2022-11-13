fda5762da9900061bb14325e52f7254e505f17a6
AleksGrig <ullitap@gmail.com>
2022-11-13 15:02:03 +0300

    screenshots

---
+++ screenshots.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import os
+
+
+current_directory = os.getcwd()
+
+service_obj = Service("drivers/chromedriver_win32/chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(service=service_obj,options=options)
+driver.implicitly_wait(10)
+driver.maximize_window()
+
+driver.get("https://demo.nopcommerce.com/")
+
+# driver.save_screenshot("{}\screenshot.png".format(current_directory))
+driver.get_screenshot_as_file("{}\screenshot.png".format(current_directory))
+
+# driver.get_screenshot_as_png() # driver.get_screenshot_as_base64 saves in binary format
