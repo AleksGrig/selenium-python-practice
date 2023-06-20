6402f975f580233db2463c2193d8ee195e57f3c8
AleksGrig <ullitap@gmail.com>
2023-06-20 14:02:40 +0300

    chrome options + refactoring

--- webelements/static_tables.py
+++ webelements/static_tables.py
@@ -6,7 +6,11 @@ from selenium.webdriver.chrome.service import Service
 service_obj = Service("drivers/chromedriver_win32/chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=service_obj,options=options)
+
+# this option forces selenium not to close browser after script execution
+options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(service=service_obj, options=options)
 driver.implicitly_wait(10)
 driver.maximize_window()
 
@@ -15,25 +19,30 @@ driver.get("https://testautomationpractice.blogspot.com")
 # count number of rows and columns of a table
 rows = driver.find_elements(By.XPATH, "//table[@name='BookTable'] //tr")
 rows_count = len(rows)
-columns = driver.find_elements(By.XPATH, "//table[@name='BookTable']//tr[1]/th")
+columns = driver.find_elements(
+    By.XPATH, "//table[@name='BookTable']//tr[1]/th")
 columns_count = len(columns)
 print("rows:", rows_count, "columns:", columns_count)
 
 # read data from specific row and column(row:5, column:1)
-data = driver.find_element(By.XPATH, "//table[@name='BookTable']//tr[5]/td[1]").text
+data = driver.find_element(
+    By.XPATH, "//table[@name='BookTable']//tr[5]/td[1]").text
 print(data)
 
 # read all rows and columns data
 print("----------all data from the table----------")
 for row in range(2, rows_count+1):
-  for column in range(1, columns_count+1):
-    data = driver.find_element(By.XPATH, "//table[@name='BookTable']//tr[{}]/td[{}]".format(row, column)).text
-    print(data, " | ", sep="", end="")
-  print()
+    for column in range(1, columns_count+1):
+        data = driver.find_element(
+            By.XPATH, "//table[@name='BookTable']//tr[{}]/td[{}]".format(row, column)).text
+        print(data, " | ", sep="", end="")
+    print()
 
 # read data based on condition(author is Mukesh)
 for row in range(2, rows_count+1):
-  author = driver.find_element(By.XPATH, "//table[@name='BookTable']//tr[{}]/td[2]".format(row)).text
-  if author == 'Mukesh':
-    title = driver.find_element(By.XPATH, "//table[@name='BookTable']//tr[{}]/td[1]".format(row)).text
-    print(title, author)
+    author = driver.find_element(
+        By.XPATH, "//table[@name='BookTable']//tr[{}]/td[2]".format(row)).text
+    if author == 'Mukesh':
+        title = driver.find_element(
+            By.XPATH, "//table[@name='BookTable']//tr[{}]/td[1]".format(row)).text
+        print(title, author)
