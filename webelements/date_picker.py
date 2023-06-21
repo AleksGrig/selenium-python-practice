c48bad38e917ea6a199fe3e71abc998140ff4d92
AleksGrig <ullitap@gmail.com>
2023-06-21 14:24:09 +0300

    chrome options + refactoring

--- webelements/date_picker.py
+++ webelements/date_picker.py
@@ -6,30 +6,35 @@ from selenium.webdriver.chrome.service import Service
 service_obj = Service("drivers/chromedriver_win32/chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=service_obj,options=options)
+
+# this option forces selenium not to close browser after script execution
+options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(service=service_obj, options=options)
 driver.implicitly_wait(10)
 driver.maximize_window()
 
 driver.get("https://jqueryui.com/datepicker")
 
+# if there is only one frame on the page we can use driver.switch_to.frame(0)
 driver.switch_to.frame(driver.find_element(By.CSS_SELECTOR, ".demo-frame"))
 
 # send date directly if possible
 # driver.find_element(By.ID, "datepicker").send_keys("10/20/2021")
 
 months = {
-  "January":  0,
-  "February": 1,
-  "March":    2,
-  "April":    3,
-  "May":      4,
-  "June":     5,
-  "July":     6,
-  "August":   7,
-  "September":8,
-  "October":  9,
-  "November": 10,
-  "December": 11
+    "January":  0,
+    "February": 1,
+    "March":    2,
+    "April":    3,
+    "May":      4,
+    "June":     5,
+    "July":     6,
+    "August":   7,
+    "September": 8,
+    "October":  9,
+    "November": 10,
+    "December": 11
 }
 
 year = "2023"
@@ -55,22 +60,25 @@ driver.find_element(By.ID, "datepicker").click()
 # driver.find_element(By.LINK_TEXT, day).click()
 
 while True:
-  year_picker = driver.find_element(By.CLASS_NAME, "ui-datepicker-year").text
-  month_picker = driver.find_element(By.CLASS_NAME, "ui-datepicker-month").text
-  previous = driver.find_element(By.CSS_SELECTOR, ".ui-icon.ui-icon-circle-triangle-w")
-  next = driver.find_element(By.CSS_SELECTOR, ".ui-icon.ui-icon-circle-triangle-e")
+    year_picker = driver.find_element(By.CLASS_NAME, "ui-datepicker-year").text
+    month_picker = driver.find_element(
+        By.CLASS_NAME, "ui-datepicker-month").text
+    previous = driver.find_element(
+        By.CSS_SELECTOR, ".ui-icon.ui-icon-circle-triangle-w")
+    next = driver.find_element(
+        By.CSS_SELECTOR, ".ui-icon.ui-icon-circle-triangle-e")
 
-  if year == year_picker and month == month_picker:
-    break
-  else:
-    if int(year_picker) > int(year):
-      previous.click()
-    elif int(year_picker) < int(year):
-      next.click()
+    if year == year_picker and month == month_picker:
+        break
     else:
-      if months.get(driver.find_element(By.CLASS_NAME, "ui-datepicker-month").text) > months.get(month):
-        previous.click()
-      else:
-        next.click()
+        if int(year_picker) > int(year):
+            previous.click()
+        elif int(year_picker) < int(year):
+            next.click()
+        else:
+            if months.get(driver.find_element(By.CLASS_NAME, "ui-datepicker-month").text) > months.get(month):
+                previous.click()
+            else:
+                next.click()
 
 driver.find_element(By.LINK_TEXT, day).click()
--- webelements/date_picker2.py
+++ webelements/date_picker2.py
@@ -7,7 +7,11 @@ from selenium.webdriver.support.select import Select
 service_obj = Service("drivers/chromedriver_win32/chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=service_obj,options=options)
+
+# this option forces selenium not to close browser after script execution
+options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(service=service_obj, options=options)
 driver.implicitly_wait(10)
 driver.maximize_window()
 
@@ -17,6 +21,7 @@ driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application")
 driver.find_element(By.ID, "dob").click()
 year_select = Select(driver.find_element(By.CLASS_NAME, "ui-datepicker-year"))
 year_select.select_by_visible_text("1966")
-month_select = Select(driver.find_element(By.CLASS_NAME, "ui-datepicker-month"))
+month_select = Select(driver.find_element(
+    By.CLASS_NAME, "ui-datepicker-month"))
 month_select.select_by_visible_text("Nov")
 driver.find_element(By.LINK_TEXT, "22").click()
