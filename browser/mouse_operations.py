f5c04105dc48a760ed06b99ef4775bb629216d16
AleksGrig <ullitap@gmail.com>
2023-06-23 11:05:49 +0300

    chrome options + refactoring

--- browser/mouse_operations.py
+++ browser/mouse_operations.py
@@ -8,19 +8,25 @@ from time import sleep
 service_obj = Service("drivers/chromedriver_win32/chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=service_obj,options=options)
+
+# this option forces selenium not to close browser after script execution
+options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(service=service_obj, options=options)
 driver.implicitly_wait(10)
 driver.maximize_window()
 
 
 # mouse hover action
-# driver.get("https://demo.automationtesting.in/Register.html")
-# interactions = driver.find_element(By.XPATH, "//a[contains(text(),'Interactions')]")
-# drag_n_drop = driver.find_element(By.XPATH, "//a[contains(text(),'Drag and Drop')]")
-# static = driver.find_element(By.XPATH, "//a[contains(text(),'Static')]")
-
-# actions = ActionChains(driver)
-# actions.move_to_element(interactions).move_to_element(drag_n_drop).move_to_element(static).click().perform()
+driver.get("https://demo.automationtesting.in/Register.html")
+interactions = driver.find_element(
+    By.XPATH, "//a[contains(text(),'Interactions')]")
+drag_n_drop = driver.find_element(
+    By.XPATH, "//a[contains(text(),'Drag and Drop')]")
+static = driver.find_element(By.XPATH, "//a[contains(text(),'Static')]")
+actions = ActionChains(driver)
+actions.move_to_element(interactions).move_to_element(
+    drag_n_drop).move_to_element(static).click().perform()
 
 
 # right click action
@@ -31,10 +37,12 @@ driver.maximize_window()
 
 
 # double click action
-# driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick3")
+# driver.get(
+#     "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick3")
 # driver.switch_to.frame("iframeResult")
 # txt_field = driver.find_element(By.ID, "field1")
-# txt_field.clear().send_keys("Try!!!")
+# txt_field.clear()
+# txt_field.send_keys("Try!!!")
 # btn = driver.find_element(By.XPATH, "//button[text()='Copy Text']")
 # actions = ActionChains(driver)
 # actions.double_click(btn).perform()
@@ -53,19 +61,22 @@ driver.maximize_window()
 # sliders = driver.find_elements(By.CSS_SELECTOR, ".ui-slider-handle")
 # left = 0
 # right = 1
-# print(sliders[left].location, sliders[right].location) # left x: 59, right x: 510
+
+# # left x: 59, right x: 510
+# print(sliders[left].location, sliders[right].location)
 # actions = ActionChains(driver)
 # actions.drag_and_drop_by_offset(sliders[left], 200, 0).perform()
 # actions.drag_and_drop_by_offset(sliders[right], -200, 0).perform()
 
 
 # scrolling page
-driver.get("https://www.countries-ofthe-world.com/flags-of-the-world.html")
+# driver.get("https://www.countries-ofthe-world.com/flags-of-the-world.html")
 
 # scrolling by pixel number
-# driver.execute_script("window.scrollBy(0,3000)","")
+# driver.execute_script("window.scrollBy(0,3000)", "")
 # y_offset = driver.execute_script("return window.pageYOffset;")
 # print(y_offset)
+
 
 # scrolling till element is visible
 # india_flag = driver.find_element(By.XPATH, "//img[@alt='Flag of India']")
@@ -73,11 +84,13 @@ driver.get("https://www.countries-ofthe-world.com/flags-of-the-world.html")
 # y_offset = driver.execute_script("return window.pageYOffset;")
 # print(y_offset)
 
+
 # scrolling till the end of the page
-driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-y_offset = driver.execute_script("return window.pageYOffset;")
-print(y_offset)
+# driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+# y_offset = driver.execute_script("return window.pageYOffset;")
+# print(y_offset)
+
 
 # scrolling up to the starting position
-sleep(3)
-driver.execute_script("window.scrollBy(0,-document.body.scrollHeight)")
+# sleep(3)
+# driver.execute_script("window.scrollBy(0,-document.body.scrollHeight)")
