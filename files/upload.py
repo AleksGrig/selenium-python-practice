8354eb1252933f5da24968d5b0f6f588f7e82a3b
AleksGrig <ullitap@gmail.com>
2023-06-25 15:27:38 +0300

    refactoring

--- files/upload.py
+++ files/upload.py
@@ -9,10 +9,13 @@ current_directory = os.getcwd()
 service_obj = Service("drivers/chromedriver_win32/chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=service_obj,options=options)
+options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(service=service_obj, options=options)
 driver.implicitly_wait(10)
 driver.maximize_window()
 
 driver.get("https://www.monsterindia.com/")
 driver.find_element(By.XPATH, "//span[text()='Upload Resume']").click()
-driver.find_element(By.ID, "file-upload").send_keys("{}\downloads\\file-sample_100kB.doc".format(current_directory))
+driver.find_element(By.ID, "file-upload").send_keys(
+    "{}\downloads\\file-sample_100kB.doc".format(current_directory))
