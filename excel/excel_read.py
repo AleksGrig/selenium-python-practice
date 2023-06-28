998cb52ce22d2c92fed596a258f3cf9a8f6829cb
AleksGrig <ullitap@gmail.com>
2023-06-28 16:16:03 +0300

    refactoring

--- excel/excel_read.py
+++ excel/excel_read.py
@@ -9,10 +9,10 @@ file = "{}\excel\\book1.xlsx".format(current_folder)
 workbook = openpyxl.load_workbook(file)
 sheet = workbook["Sheet1"]
 
-row_number = sheet.max_row 
+row_number = sheet.max_row
 column_number = sheet.max_column
 
 for row in range(1, row_number+1):
-  for column in range(1, column_number+1):
-    print(sheet.cell(row, column).value, end=" ")
-  print()
+    for column in range(1, column_number+1):
+        print(sheet.cell(row, column).value, end=" ")
+    print()
--- excel/excel_write.py
+++ excel/excel_write.py
@@ -6,11 +6,11 @@ current_folder = os.getcwd()
 file = "{}\excel\\empty.xlsx".format(current_folder)
 
 workbook = openpyxl.load_workbook(file)
-sheet = workbook["Sheet1"] # or workbook.active
+sheet = workbook["Sheet1"]  # or workbook.active
 
 # same data
 for row in range(1, 6):
-  for column in range(1, 4):
-    sheet.cell(row, column).value = "welcome"
+    for column in range(1, 4):
+        sheet.cell(row, column).value = "welcome"
 
 workbook.save(file)
